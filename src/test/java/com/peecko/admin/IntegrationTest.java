fecec1bf92c36a58bce24d8e90089cc1783f23e2
jpleitez <jose.pleitez@gmail.com>
2023-01-01 22:08:35 +0100

    add generated application

---
+++ src/test/java/com/peecko/admin/IntegrationTest.java
@@ -0,0 +1,23 @@
+package com.peecko.admin;
+
+import com.peecko.admin.PeeckoAdminApp;
+import com.peecko.admin.config.AsyncSyncConfiguration;
+import com.peecko.admin.config.EmbeddedSQL;
+import com.peecko.admin.config.TestSecurityConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { PeeckoAdminApp.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
